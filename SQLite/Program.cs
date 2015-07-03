4f7363e2f68d025a7de5666a91c7e7afc8a5c002
SummerPraktika <anton.ankilov@mail.ru>
2015-07-03 23:31:42 +0300

    Add entity Framework Add method Add record

---
+++ SQLite/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data.Entity;
+using System.Data.Common;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SQLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MyDBContext())
+            {
+                db.Templetes.Add(new Templete { Name = "Test" });
+                db.SaveChanges();
+            }
+            using (var db = new MyDBContext())
+            {
+                foreach (var note in db.Templetes)
+                {
+                    Console.WriteLine("Template {0} = {1}", note.Id, note.Name);
+                }
+            }
+            Console.ReadKey();
+        }
+        public class Templete
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+         public class MyDBContext : DbContext
+         {
+             public MyDBContext() : base("MyDBContext")
+             {
+             }
+             public DbSet<Templete> Templetes { get; set; }
+         }
+    }
+}
